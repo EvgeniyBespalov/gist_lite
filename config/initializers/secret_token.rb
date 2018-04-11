8e19ae7737e04e1e10950528a8670d8fff61ee9b
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-11 13:23:37 +0300

    precompile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GistLite::Application.config.secret_key_base = 'a4afcde635613aeb5b2ad3664d931fb91c6784f58f656ce905499919c96a3167871a046cfe05a9bba7b651dbe96042bbcfe9c278f0f6920ba1afacc26199a0b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GistLite::Application.config.session_store :cookie_store, key: '_gist_lite_session'
