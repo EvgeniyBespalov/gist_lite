069543850928b335fc985e24cfc20a3ad0e8115a
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-13 13:19:57 +0300

    create model comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411104419) do
+ActiveRecord::Schema.define(version: 20180413101403) do
+
+  create_table "comments", force: true do |t|
+    t.string   "text"
+    t.integer  "gist_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "gists", force: true do |t|
     t.string   "file_name"
