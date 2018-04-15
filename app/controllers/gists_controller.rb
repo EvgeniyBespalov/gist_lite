dc358e4fdc8555706d794afb8adb3ebce1ecde2a
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-15 22:43:14 +0300

    table remove from html

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,7 +1,8 @@
 class GistsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
   before_action :set_gist, only: [:show, :edit, :update, :destroy]
-
+  before_action :user_valid, only: [:update, :destroy]
+  
   respond_to :html
 
   def index
@@ -29,36 +30,34 @@ class GistsController < ApplicationController
     end
   end
 
-  def update
-    if current_user == @gist.user
-      unless @gist.update(gist_params)
-        render "edit"
-        return
-      end
+  def update    
+    if @gist.update(gist_params)
+      redirect_to gist_path(@gist)
     else
-      flash[:error] = "Access denied"
-    end   
-    
-    redirect_to gist_path(@gist)
+      render "edit"
+    end
   end
 
   def destroy
-    if current_user == @gist.user
-      @gist.destroy
-      redirect_to gists_path
-    else
-      flash[:error] = "Access denied"
-      redirect_to gist_path(@gist)
-    end
+    @gist.destroy
+    redirect_to gists_path
   end
 
   private
     
-    def set_gist
-      @gist = Gist.find(params[:id])
-    end
+  def set_gist
+    @gist = Gist.find(params[:id])
+  end
 
-    def gist_params
-      params.require(:gist).permit(:file_name, :text)
+  def gist_params
+    params.require(:gist).permit(:file_name, :text)
+  end
+  
+  def user_valid
+    if current_user != @gist.user
+      flash[:error] = "Access denied"
+      redirect_to gist_path(@gist)
+      return
     end
+  end
 end
