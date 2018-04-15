4796a7e213bebf625b55d67255cf730ffaff7bbd
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-15 17:55:14 +0300

    small change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,17 @@
 class CommentsController < ApplicationController
-  before_action :set_gist
+  before_filter :authenticate_user!, only: [:create]
+  before_action :set_gist, only: [:create]
   
   def create
-    if current_user
-      text = params[:comment]
-      
-      comment = Comment.new
-      comment.text = text
-      comment.user = current_user
-      comment.gist = @gist
-      
-      comment.save
-      
-    end
+    text = params[:comment]
+    
+    comment = Comment.new
+    comment.text = text
+    comment.user = current_user
+    comment.gist = @gist
+    
+    comment.save
+          
     respond_to do |format|
       format.json { render json: {comment: comment, user_email: current_user.email}}
     end
